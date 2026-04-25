12db543a6048b8bf091b98e0b5d35c8efd45de82
alessandroamormino <amormino.alessandro97@gmail.com>
2026-04-25 13:46:15 +0200

    feat: enable heimdall logs

--- components/HeimdallWrapper.tsx
+++ components/HeimdallWrapper.tsx
@@ -2,9 +2,15 @@
 
 import { HeimdallProvider } from 'heimdall-sdk'
 
+const COLLECTOR_URL = process.env.NEXT_PUBLIC_HEIMDALL_URL ?? 'http://localhost:3333'
+
 export default function HeimdallWrapper({ children }: { children: React.ReactNode }) {
   return (
-    <HeimdallProvider collectorUrl="http://localhost:3333" project="fuorirotta">
+    <HeimdallProvider
+      collectorUrl={COLLECTOR_URL}
+      project="fuorirotta"
+      enabled={!!process.env.NEXT_PUBLIC_HEIMDALL_URL}
+    >
       {children}
     </HeimdallProvider>
   )
